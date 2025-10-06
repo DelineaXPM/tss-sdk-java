024f0aac18f3be68e9cde24e15ff00eb55d7693e
delinea-rajani <164006534+delinea-rajani@users.noreply.github.com>
2025-10-06 07:16:25 -0400

    Update VaultsResponseModel.java

--- src/main/java/com/delinea/secrets/server/spring/VaultsResponseModel.java
+++ src/main/java/com/delinea/secrets/server/spring/VaultsResponseModel.java
@@ -6,8 +6,9 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
 public class VaultsResponseModel {
 
     @JsonProperty("vaults")
@@ -21,7 +22,8 @@ public class VaultsResponseModel {
         this.vaults = (vaults == null) ? new ArrayList<>() : new ArrayList<>(vaults);
     }
 
-    @Data
+    @Setter
+    @Getter
     public static class Vault {
         @JsonProperty("vaultId")
         private String vaultId;
